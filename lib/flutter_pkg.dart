e0e7ee4b2285975c13b48f100bfd823f0a4f5b89
Rafael Oliveira <rafael.oliveira@w16.dev>
2024-06-24 12:40:28 -0300

    feat:added kelvin converter

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -1,7 +1,5 @@
 library flutter_pkg;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class KelvinToCelcius {
+  int convert(int value) => value - 273;
 }
